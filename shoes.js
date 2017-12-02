e1fe01006f20c5c1b14c95cd0006ba38417cf465
Yesica Ramirez <yesmagaly@gmail.com>
2017-12-02 14:57:00 -0500

    Added constraints validation.

--- shoes.js
+++ shoes.js
@@ -3,6 +3,17 @@ function pairOfShoes(shoes) {
   var rightShoes = [];
 
   // 1 ≤ shoes.length ≤ 25
+  if (shoes.length < 1 || shoes.length > 25) {
+    throw new Error('The number of shoes exceded the allowed quantity!')
+  }
+
+  const areValidSizes = shoes.every((shoe) => {
+    return shoe[1] <= 100 && shoe[1] >= 1
+  })
+  
+  if (!areValidSizes) {
+    throw new Error('One of the shoe has a invalid size')
+  }
 
   for (var i = 0; i < shoes.length; i++) {
     if (shoes[i][0] === 0) {
--- tests/shoes.test.js
+++ tests/shoes.test.js
@@ -33,4 +33,42 @@ describe('pairOfShoes', () => {
 
     expect(pairOfShoes(arr)).toBe(true);
   });
+
+  it('should not exceute if there are more of 25 shoes', () => {
+    const arr = [
+      [0,23],
+      [1,21],
+      [1,23],
+      [0,21],
+      [0,21],
+      [0,23],
+      [1,21],
+      [1,23],
+      [0,21],
+      [0,21],
+      [0,23],
+      [1,21],
+      [1,23],
+      [0,21],
+      [0,21],
+      [0,23],
+      [1,21],
+      [1,23],
+      [0,21],
+      [0,21],
+      [0,23],
+      [1,21],
+      [1,23],
+      [0,21],
+      [0,21],
+      [0,21]
+    ]
+
+    expect(() => {pairOfShoes(arr)} ).toThrow('The number of shoes exceded the allowed quantity!');
+  });
+
+  it('should not excute when the size excedes 100 size', () => {
+    const arr = [[1, 210], [0, 210]]
+    expect(() => {pairOfShoes(arr)} ).toThrow('One of the shoe has a invalid size');
+  });
 });
